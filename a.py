04d04233d52ec5462630071082a95a89a389ac79
negikaithal <negikaithal@gmail.com>
2019-08-28 19:54:22 +0530

    Add files via upload

---
+++ a.py
@@ -0,0 +1,27 @@
+import wx
+import wx.grid as gridlib
+
+
+########################################################################
+class MyForm(wx.Frame):
+    """"""
+
+    # ----------------------------------------------------------------------
+    def __init__(self):
+        """Constructor"""
+        wx.Frame.__init__(self, parent=None, title="A Simple Grid")
+        panel = wx.Panel(self)
+
+        myGrid = gridlib.Grid(panel)
+        myGrid.CreateGrid(12, 8)
+
+        sizer = wx.BoxSizer(wx.HORIZONTAL)
+        sizer.Add(myGrid, 1, wx.EXPAND)
+        panel.SetSizer(sizer)
+
+
+if __name__ == "__main__":
+    print(__name__)
+    app = wx.App()
+    frame = MyForm().Show()
+    app.MainLoop()
